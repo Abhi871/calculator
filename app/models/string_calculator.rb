d4007ebe330c730103109cda366e0f6273fa9bbc
Abhi871 <asengar871@gmail.com>
2024-12-08 19:07:07 +0530

    Added logic for custom delimeter

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -2,7 +2,20 @@ class StringCalculator < ApplicationRecord
   def self.add(numbers)
     return 0 if numbers.empty?
 
+    # Default delimiter is comma or newline
     delimiter = /,|\n/
+
+    if numbers.start_with?("//")
+      # If the first line starts with "//", it contains the delimiter information
+      first_line, numbers = numbers.split("\n", 2)
+
+      # Extract the delimiter from the first line
+      delimiter = Regexp.escape(first_line[2])
+
+      delimiter = delimiter + '|\n'
+    end
+
+    # Split the input string by the delimiter and convert to integers and sum them
     numbers.split(/#{delimiter}/).map(&:to_i).sum
   end
 end
--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -18,8 +18,16 @@ RSpec.describe StringCalculator, type: :model do
       expect(StringCalculator.add("1,2,3,4")).to eq(10)
     end
 
-    it "returns the sum of all the numbers" do
+    it "returns the sum of all the numbers with new lines" do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
+    it "supports custom delimiters" do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
+
+    it "supports custom delimiters with multiple new lines" do
+      expect(StringCalculator.add("//;\n1;2\n3\n4;5")).to eq(15)
     end
   end
 end
