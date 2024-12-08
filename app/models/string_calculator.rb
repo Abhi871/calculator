94a86fa75a9f7a7312e917e1876b8e6f41e1397c
Abhi871 <asengar871@gmail.com>
2024-12-08 19:15:05 +0530

    Raising error for negative numbers

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -12,10 +12,24 @@ class StringCalculator < ApplicationRecord
       # Extract the delimiter from the first line
       delimiter = Regexp.escape(first_line[2])
 
+      # Handle the case where the delimiter is a newline
       delimiter = delimiter + '|\n'
     end
 
-    # Split the input string by the delimiter and convert to integers and sum them
-    numbers.split(/#{delimiter}/).map(&:to_i).sum
+    # # Split the input string by the delimiter and convert to integers and sum them
+    # numbers.split(/#{delimiter}/).map(&:to_i).sum
+
+    # Split the input string by the delimiter and convert to integers
+    nums = numbers.split(Regexp.new(delimiter)).map(&:to_i)
+
+    # Check for negative numbers
+    negatives = nums.select(&:negative?)
+    
+    # If there are negative numbers, raise an error
+    if negatives.any?
+      raise "negative numbers not allowed: #{negatives.join(', ')}"
+    end
+
+    nums.sum
   end
 end
--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -29,5 +29,9 @@ RSpec.describe StringCalculator, type: :model do
     it "supports custom delimiters with multiple new lines" do
       expect(StringCalculator.add("//;\n1;2\n3\n4;5")).to eq(15)
     end
+
+    it "raises an error for negative numbers" do
+      expect { StringCalculator.add("1,-2,3,-4") }.to raise_error("negative numbers not allowed: -2, -4")
+    end
   end
 end
